8a2a254c5164cb22c3348c7eeee76d20f14ee0c9
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2024-02-16 19:14:37 +0530

    Add files via upload

---
+++ AnnuityPurchasedByRepository.java
@@ -0,0 +1,20 @@
+package com.lic.epgs.mst.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.lic.epgs.mst.entity.AddressType;
+import com.lic.epgs.mst.entity.AnnuityPurchasedBy;
+
+@Repository
+public interface AnnuityPurchasedByRepository extends JpaRepository<AnnuityPurchasedBy, Long> {
+	
+	@Query("SELECT DISTINCT a FROM AnnuityPurchasedBy a WHERE a.annuityCode = :code")
+	public Optional<AnnuityPurchasedBy> findByCode(String code);
+
+	@Procedure(name = "GRATVALUATION_NEW")
+	public void calculateGrat();
+}
---
+++ AnnuityPurchasedByService.java
@@ -0,0 +1,17 @@
+package com.lic.epgs.mst.service;
+
+import java.util.List;
+
+import com.lic.epgs.mst.entity.AnnuityPurchasedBy;
+
+public interface AnnuityPurchasedByService {
+
+	List<AnnuityPurchasedBy> getAllAnnuityPurchasedBy();
+
+	public AnnuityPurchasedBy getAnnuityPurchasedByById(long id);
+
+	public AnnuityPurchasedBy findByCode(String code);
+
+	public void calculateGrat();
+
+}
