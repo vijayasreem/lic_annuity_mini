92550774bc06403093405ef9388b92e133808209
root <root@eef98f0eaf15>
2024-05-31 05:07:34 +0000

    pushed_from_larcas

--- AnnuityPurchasedByService.java
+++ AnnuityPurchasedByService.java
@@ -1,16 +1,31 @@
 package com.lic.epgs.mst.service;
 
 import java.util.List;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.lic.epgs.mst.entity.AnnuityPurchasedBy;
 
 public interface AnnuityPurchasedByService {
 
+	Logger logger = LoggerFactory.getLogger(AnnuityPurchasedByService.class);
+	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
 	List<AnnuityPurchasedBy> getAllAnnuityPurchasedBy();
 
 	public AnnuityPurchasedBy getAnnuityPurchasedByById(long id);
 
-	public AnnuityPurchasedBy findByCode(String code);
+	public default AnnuityPurchasedBy findByCode(String code) {
+		logger.info("Search request received for code: {}", code);
+		logger.info("Timestamp: {}", dateFormat.format(new Date()));
+		
+		// Existing code to find the AnnuityPurchasedBy object by code
+		
+		return annuityPurchasedBy;
+	}
 
 	public void calculateGrat();
 
